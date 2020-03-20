001ebf372ba797b5f55c5479f56e46655466af03
Greg Dobler <greg.dobler@gmail.com>
2020-03-20 14:45:07 -0400

    read fits

--- py/utils.py
+++ py/utils.py
@@ -3,7 +3,9 @@
 
 import os
 import glob
+from astropy.io import fits
 from config import *
+
 
 def get_file_list(stype=None, imtype="gif"):
     """
@@ -17,3 +19,15 @@ def get_file_list(stype=None, imtype="gif"):
         return [i for i in flist if stype in i]
     else:
         return flist
+
+
+def fits2stamp(fname):
+    """
+    Pull the postage stamp from a fits file.
+    (MAKES ASSUMPTIONS ABOUT FITS HDU LAYOUT)
+
+    Returns:
+        numpy.ndarray
+    """
+
+    return fits.open(fname)[0].data
