b27e132cb19cfc681b940604dda0af9b88a35d5c
Greg Dobler <greg.dobler@gmail.com>
2020-03-20 13:44:11 -0400

    change figure spacing

--- py/view_images.py
+++ py/view_images.py
@@ -8,9 +8,10 @@ from config import *
 
 plt.style.use("bmh")
 
-flist = glob.glob(os.path.join(configs["dpath"], "20140807-r946", "*.gif"))
+flist = glob.glob(os.path.join(configs["dpath"], "*", "*.gif"))
 
 fig, ax = plt.subplots(5, 5, figsize=[7, 7])
+fig.subplots_adjust(0.05, 0.05, 0.95, 0.95, 0.05, 0.05)
 
 for ii in range(5):
     for jj in range(5):
