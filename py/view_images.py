cb5a24dd0b2586b2a8625356b610e6cd8bc46985
Greg Dobler <greg.dobler@gmail.com>
2020-03-20 13:39:01 -0400

    use config file

--- py/view_images.py
+++ py/view_images.py
@@ -1,19 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
 import matplotlib.pyplot as plt
 import glob
+from config import *
 
 plt.style.use("bmh")
 
-flist = glob.glob("../data/stamps_10/stamps/20140807-r946/*.gif")
+flist = glob.glob(os.path.join(configs["dpath"], "20140807-r946", "*.gif"))
 
 fig, ax = plt.subplots(5, 5, figsize=[7, 7])
-for ii in range(5):
-    for jj in range(5):
-        ax[ii, jj].axis("off")
-        ax[ii, jj].imshow(plt.imread(flist[5 * ii] + jj))
+
 for ii in range(5):
     for jj in range(5):
         ax[ii, jj].axis("off")
         ax[ii, jj].imshow(plt.imread(flist[5 * ii + jj]))
+
+plt.show()
