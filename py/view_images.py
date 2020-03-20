3dbcd34a4b5a93cf59d41882ebd1af6d0715c547
Greg Dobler <greg.dobler@gmail.com>
2020-03-20 13:46:53 -0400

    add file names

--- py/view_images.py
+++ py/view_images.py
@@ -11,11 +11,14 @@ plt.style.use("bmh")
 flist = glob.glob(os.path.join(configs["dpath"], "*", "*.gif"))
 
 fig, ax = plt.subplots(5, 5, figsize=[7, 7])
-fig.subplots_adjust(0.05, 0.05, 0.95, 0.95, 0.05, 0.05)
+fig.subplots_adjust(0.05, 0.05, 0.95, 0.95, 0.05, 0.25)
 
 for ii in range(5):
     for jj in range(5):
+        ind = 5 * ii + jj
+
         ax[ii, jj].axis("off")
-        ax[ii, jj].imshow(plt.imread(flist[5 * ii + jj]))
+        ax[ii, jj].imshow(plt.imread(flist[ind]))
+        ax[ii, jj].set_title(os.path.split(flist[ind])[-1], fontsize=10)
 
 plt.show()
